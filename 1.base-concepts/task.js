dc68bfa94771a5b78483ad90db57c82f8cf9d307
Marina Drugina <druginamarina@gmail.com>
2022-07-09 16:34:20 +0300

    script for task #1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,45 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+
+  let D;
+  D = Math.pow(b, 2) - 4*a*c;
+  
+  if (D < 0) {
+    arr = [];
+  } else if (D === 0) {
+    arr = [-b / (2*a)];
+  } else if (D > 0) {
+    arr = [(-b + Math.sqrt(D)) / (2*a), (-b - Math.sqrt(D)) / (2*a)];
+  }
+
   return arr; // array
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  
+  if (isNaN(+percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(+contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(+amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+  
+  let primaryLoan = amount - contribution;
+  
+  let dateNow = new Date(); 
+  const oneMonth = 1000 * 60 * 60 * 24 * 30; 
+  let dateAll = Math.floor((date.getTime() - dateNow.getTime()) / oneMonth); 
+  
+  let P = percent / 100 / 12;
+  let payment = primaryLoan * (P + (P / (((1 + P) ** dateAll) - 1)));
+  totalAmount = Math.round((payment * dateAll) * 100) / 100;
+  
   return totalAmount;
+  }
 }
