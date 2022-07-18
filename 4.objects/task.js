cd7eeddf80b8268f99569cb44a4347ca8278787d
Marina Drugina <druginamarina@gmail.com>
2022-07-18 19:42:19 +0300

    Script for task #4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,27 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks === undefined ? this.marks = [mark] : this.marks.push(mark); 
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a,b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
