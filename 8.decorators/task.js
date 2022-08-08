6b7fcd07d1d2be3e79c0b9dee98560806d0526fb
Marina Drugina <druginamarina@gmail.com>
2022-08-08 20:01:44 +0300

    Script for task #8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,59 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+
+    let objectInCache = cache.find(item => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+        
+    if (objectInCache) { // если элемент найден
+      console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + objectInCache.value;
+    }  
+
+    let result = func(...args);// в кэше результата нет - придётся считать 
+        
+    cache.push({hash: hash, value: result}) ; // добавляем элемент с правильной структурой
+        
+    if (cache.length > 5) { 
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+        
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;    
+  }    
+
+  return wrapper;  
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  return function() {
+    
+    timeoutId ? clearTimeout(timeoutId) : console.log(func());
+
+    timeoutId = setTimeout(() => console.log(func()), delay);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+
+function debounceDecorator2(func, delay) {
+  let timeoutId = null;
+        
+  function wrapper() {
+    wrapper.count++;
+        
+    timeoutId ? clearTimeout(timeoutId) : console.log(func());
+    
+    timeoutId = setTimeout(() => console.log(func()), delay);
+    
+    //console.log(wrapper.count);
+    }
+
+  wrapper.count = 0;
+  
+  return wrapper;
 }
